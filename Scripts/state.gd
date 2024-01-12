09ed02634cf111995949c3e10bc05be83bcbb232
Jiahua Ma <naxxhua@gmail.com>
2024-01-11 23:54:26 -0600

    feat: finished the structure of finite state machine

---
+++ Scripts/state.gd
@@ -0,0 +1,22 @@
+extends Node2D
+class_name State
+
+@onready var debug = owner.find_child("debug")
+@onready var player = owner.get_parent().find_child("Player")
+@onready var animation_player = owner.find_child("AnimationPlayer")
+
+func _ready():
+	set_physics_process(false)
+
+func enter():
+	set_physics_process(true)
+
+func exit():
+	set_physics_process(false)
+
+func transition():
+	pass
+
+func _physics_process(delta):
+	transition()
+	debug.text = name
