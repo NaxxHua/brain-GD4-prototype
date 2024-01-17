ad2d88aa3c8a5dc46435f863650414ae76982a0c
Jiahua Ma <naxxhua@gmail.com>
2024-01-16 23:48:45 -0600

    feat: added dash state

---
+++ Scripts/Dash.gd
@@ -0,0 +1,19 @@
+extends State
+
+var can_transition: bool = false
+
+func enter():
+	super.enter()
+	animation_player.play("glowing")
+	await dash()
+	can_transition = true
+
+func dash():
+	var tween = create_tween()
+	tween.tween_property(owner, "position", player.position, 0.8)
+	await tween.finished
+
+func transition():
+	if can_transition:
+		can_transition = false
+		get_parent().change_state("Follow")
